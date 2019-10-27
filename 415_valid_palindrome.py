0ee296cf7ff0e41769fdab4b66dcf9f471f485e5
Hao <haohu.2011@hotmail.com>
2019-10-27 19:53:15 +0800

    415 - valid palindrome: 1.0

--- 415_valid_palindrome.py
+++ 415_valid_palindrome.py
@@ -1,27 +1,46 @@
 """LintCode No.415 Valid Palindrome
 
-Functions will be tested under LintCode test environment
 """
 
 
-def is_valid_palindrome(input_str):
-    left, right = 0, len(input_str) - 1
+class Solution:
+    """
+    @param s: A string
+    @return: Whether the string is a valid palindrome
+    """
 
-    while left < right and not input_str[left].isalpha() and not input_str[left].isdigit():
-        left += 1
-    while left < right and not input_str[right].isalpha() and not input_str[right].isdigit():
-        right += 1
+    def is_palindrome(self, s):
+        self.is_not_used()
+        s = s.lower()
+        left, right = 0, len(s) - 1
 
-    while left < right:
-        if input_str[left].lower() == input_str[right].lower():
-            continue
-        else:
-            return False
+        # '' or ' ' makes left >= right(e.g. 0 > -1 or 0 = 0), and return True
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
 
-    return True
+            if s[left] != s[right]:
+                return False
 
+            left += 1
+            right -= 1
 
+        return True
+
+    # PyCharm "thinks" that you might have wanted to have a static method, but you forgot to declare it to be static.
+    #
+    # is_palindrome method does not use self in its body and hence does not actually change the class instance.
+    # Hence the method could be static.
+    def is_not_used(self):
+        pass
+
+# let's write some test cases
+sol = Solution()
 a_str = 'A man, a plan, a canal: Panama'
-valid = is_valid_palindrome(a_str)
-print(valid)
+
+print(sol.is_palindrome(s=' '))
 
