2b0605da947721510edb8fcc5ca1fab626fa7cb0
Hao <haohu.2011@hotmail.com>
2019-10-28 22:53:21 +0800

    17 - subSets I: 1.0

---
+++ 18_subSets_II.py
@@ -0,0 +1,11 @@
+"""LintCode No.18 Subsets II
+
+"""
+
+class Solution:
+    """
+    @param nums: A set of numbers.
+    @return: A list of lists. All valid subsets.
+    """
+    def subsets_with_dup(self, nums):
+        
